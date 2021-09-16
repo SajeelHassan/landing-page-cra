e027aac1e72136d3d732fd68f8c5c599ea75b730
Sajeel Hassan <hsajeel.dev@gmail.com>
2021-09-16 15:31:13 +0500

    lcoation changed

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -16,8 +16,8 @@ const Product = ({ id, title, price, artist, front, back }) => {
         showArrows={true}
         className={classes.image}
       >
-        <img src={`/coins/${front}`} alt="coin" />
-        <img src={`/coins/${back}`} alt="coin" />
+        <img src={`/coins-small/${front}`} alt="coin" />
+        <img src={`/coins-small/${back}`} alt="coin" />
       </Carousel>
       <div className={classes.details}>
         <div className={classes.titleAuthor}>
@@ -32,7 +32,7 @@ const Product = ({ id, title, price, artist, front, back }) => {
             data-item-price={price.slice(1)}
             data-item-url="https://artmint.com/"
             data-item-description="High-quality gold products"
-            data-item-image={`/coins/${front}`}
+            data-item-image={`/coins-small/${front}`}
             data-item-name={title}
           >
             ADD TO CART
